afb67f42e3ed7ddd4461fe87c52d98393b8e6584
Modena <felipe.kroitor@gmail.com>
2021-07-21 12:09:25 -0300

    Update paginas.vue

--- client-app/pages/paginas.vue
+++ client-app/pages/paginas.vue
@@ -3,21 +3,95 @@
     <h3>Páginas</h3>
     <div id="paginas-lista-paginas" class="px-2 py-1">
       <b-container>
-        <b-row>
-          <b-col><h5><strong>Página</strong></h5> </b-col>
-          <b-col><h5><strong>Url</strong></h5></b-col>
-          <b-col></b-col>
-        </b-row>
-        <b-row v-for="(pag, index) in paginasC" :key="index">
-          <b-col>{{ pag.nomePagina }}</b-col>
-          <b-col>Url</b-col>
-          <b-col >
-              <p class="float-right">{{pag.componente.length}}</p>
-              
+        <b-row class="border-bottom mt-2 mb-3">
+          <b-col
+            ><h5><strong>Página</strong></h5>
           </b-col>
+          <b-col
+            ><h5><strong>Url</strong></h5></b-col
+          >
+          <b-col><strong>Ação</strong></b-col>
         </b-row>
+        <div v-for="(pag, index) in paginasC" :key="index">
+          <b-row>
+            <b-col
+              class="mouse-hover-icone-select"
+              @click="abreColapse('collapse-componentes-' + pag.nomePagina)"
+              ><strong
+                ><p>{{ pag.nomePagina }}</p></strong
+              >
+            </b-col>
+            <b-col>{{ pag.url }}</b-col>
+            <b-col class="d-flex">
+              <img
+                width="18px"
+                height="18px"
+                src="~/assets/icones/editar.png"
+                alt=""
+                class="mouse-hover-icone-select"
+                @click="openModalPaginas(pag)"
+              />
+              <img
+                class="float-right ml-4 mouse-hover-icone-select"
+                width="20px"
+                height="20px"
+                src="~/assets/icones/visualisar-loja-preto.png"
+                alt=""
+                @click="rotaExternaDaPagina(pag.url)"
+              />
+            </b-col>
+          </b-row>
+          <b-collapse
+            :id="'collapse-componentes-' + pag.nomePagina"
+            class="border border-dark mb-4"
+          >
+            <b-card>
+              <b-row class="cinza-claro">
+                <b-col>
+                  <p>Componente</p>
+                </b-col>
+                <b-col>
+                  <p>Ação</p>
+                </b-col>
+              </b-row>
+              <b-row v-for="(componente, compI) in pag.componente" :key="compI">
+                <b-col>
+                  <p>{{ componente.tipoComponente }}</p>
+                </b-col>
+                <b-col>
+                  <img
+                    width="18px"
+                    height="18px"
+                    src="~/assets/icones/editar.png"
+                    alt="editar.png"
+                    class="mouse-hover-icone-select"
+                    @click="editarComponente(pag, componente)"
+                /></b-col>
+              </b-row>
+            </b-card>
+          </b-collapse>
+        </div>
       </b-container>
     </div>
+    <b-modal
+      id="modal-pagina-altera"
+      :title="'Configuração ' + paginaAuxModal.nomePagina"
+      hide-footer
+    >
+      <div class="configuracoes-forms ">
+        <label for="modal-pagina-altera-url"><strong>Url</strong></label>
+        <input v-model="paginaAuxModal.url" id="modal-pagina-altera-url" />
+      </div>
+      <div class="mt-4">
+        <div class="float-right">
+          <button class="btn-cancelar mr-2" @click="modalFormUrl(false)">
+            Cancelar
+          </button>
+          <button class="btn-salvar" @click="modalFormUrl(true)">Salvar</button>
+        </div>
+      </div>
+    </b-modal>
+    <PaginasModalComponentes ref="modalComponentes" />
   </div>
 </template>
 
@@ -33,13 +107,14 @@ export default {
   data() {
     return {
       paginasC: "",
+      paginaAuxModal: "",
+      componenteAConfigurar:'',
     };
   },
   watch: {
     paginas: function (newVal, oldVal) {
-      console.log(newVal);
       if (newVal != null) {
-        this.paginasC = Object.assign({}, newVal);
+        this.paginasC = JSON.parse(JSON.stringify(newVal));
       }
     },
   },
@@ -47,6 +122,34 @@ export default {
     ...mapActions({
       getPaginas: "paginas/getPaginas",
     }),
+    abreColapse(collapseSelecionado) {
+      this.$root.$emit("bv::toggle::collapse", collapseSelecionado);
+    },
+    openModalPaginas(pag) {
+      console.log(pag);
+      this.paginaAuxModal = pag;
+      this.$bvModal.show("modal-pagina-altera");
+    },
+    rotaExternaDaPagina(url) {
+      alert("emplementar rota para " + url);
+    },
+    async modalFormUrl(save) {
+      if (save == true) {
+        await this.$axios
+          .put("Paginas/updatePagina", this.paginaAuxModal)
+          .then((response) => {
+            this.getPaginas();
+            alert(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+      this.$bvModal.hide("modal-pagina-altera");
+    },
+    editarComponente(pagina,componente){
+      this.$refs.modalComponentes.getCompoenenteEPaginaId(pagina.nomePagina,componente);
+    }
   },
   created() {
     this.getPaginas();
