aba4294a2a39e941d229ae0eadc803930f9ce1bb
Modena <felipe.kroitor@gmail.com>
2021-07-22 11:51:21 -0300

    Create IComponenteBusiness.cs

---
+++ SD-WebSite-DashBoardApi/SD-WebSite-DashBoardApi/Business/IComponenteBusiness.cs
@@ -0,0 +1,14 @@
+﻿using SD_WebSite_DashBoardApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SD_WebSite_DashBoardApi.Business
+{
+    public interface IComponenteBusiness
+    {
+        object UpdateComponentes(List<Componente> componentes);
+        object UpdateComponente(Componente componente);
+    }
+}
