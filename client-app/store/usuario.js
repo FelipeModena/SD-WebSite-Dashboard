682e064be3d72b6dbce657495062c4ed7cd7aa23
Modena <felipe.kroitor@gmail.com>
2021-07-23 13:23:44 -0300

    Update usuario.js

--- client-app/store/usuario.js
+++ client-app/store/usuario.js
@@ -1,33 +1,61 @@
 import axios from 'axios'
 export const state = () => ({
-    username: "Usuario"
+  username: 'null'
 })
 
 export const getters = {
-    username(state){
-        return state.username;
-    }
+  username(state) {
+    return state.username;
+  }
 }
 
 export const actions = {
-    async Login({},user){
-        this.$axios
-        .get("administrador/login", {
-          params: {
-            user:user.username,
-            password: user.password,
-          },
-        })
-        .then((response) => {
-          console.log(response);
-          if (response.status == 200) {
-            localStorage.setItem('adminId',response.data.id);
-            this.$router.push("painel");
-            alert("Bem vindo "+ response.data);
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+  async Login({ }, user) {
+    this.$axios
+      .get("administrador/login", {
+        params: {
+          user: user.username,
+          password: user.password,
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.status == 200) {
+          localStorage.setItem('adminId', response.data.id);
+          this.$router.push("painel");
+          alert("Bem vindo " + response.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  async AutenticatorUser({ commit }) {
+    if (process.browser) {
+      if (localStorage.getItem('adminId') == null) {
+        //this.$router.push("/");
+      }
+      if (localStorage.getItem('adminId') != null) {
+        //this.$router.push("/");
+        let id = localStorage.getItem("adminId");
+        this.$axios.get("administrador",
+          {
+            params: { id: id }
+          })
+          .then(response => {
+            commit("SET_USER", response.data)
+          })
+          .catch(error => {
+            console.log(error);
+          })
+      }
     }
+  }
+}
+export const mutations = {
+  SET_USER(state, user) {
+    state.username = {};
+    state.username.nome = user.usuario;
+    state.username.id = user.id;
+  },
 }
