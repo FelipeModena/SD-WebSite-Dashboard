123a7cac1e6841bdd4f7b7d8b5f6de2e1587f033
Modena <felipe.kroitor@gmail.com>
2021-08-19 15:47:29 -0300

    Update painel.vue

--- client-app/pages/painel.vue
+++ client-app/pages/painel.vue
@@ -3,7 +3,10 @@
     <b-container fluid>
       <b-row class="text-center">
         <b-col md="1" class="py-3">
-          <b-button @click="salvarAlteracoes()" class="btn px-5 py-2" variant="success"
+          <b-button
+            @click="salvarAlteracoes()"
+            class="btn px-5 py-2"
+            variant="success"
             >Salvar Altereçoes</b-button
           >
         </b-col>
@@ -14,35 +17,67 @@
       <template v-slot:item="{ item }">
         <Drag :key="item.id" :data="item">
           <div
-            class="d-flex bg-secondary my-2"
+            class="d-flex bg-secondary mt-4"
             :class="EstiloPaginaAtiva(item.ativa)"
             :id="'b-paginas-drop-coplase-' + item.id"
           >
             <b-button
-              @click="abreColapse('collapse-paginas-' + item.id)"
+              @click="abreColapse('b-componente-drop-coplase-' + item.id)"
               variant=" w-100  p-3"
               >{{ item.nomePagina }}</b-button
             >
             <b-form-checkbox
               @change="desativaPag(item, 'b-paginas-drop-coplase-' + item.id)"
-              :checked="'' + item.ativa"
-              name="check-button"
+              :checked="item.ativa"
               switch
             >
             </b-form-checkbox>
           </div>
 
-          <b-collapse :id="'collapse-paginas-' + item.id" class="mt-2 text-left">
-            <div class="my-3 bg-primary p-1" v-for="(componente, index) in item.componente" :key="index">
-              <p class="card-text">{{ componente.tipoComponente }}</p>
-              <div class="d-flex ">
-                <b-form-checkbox :checked="'' + componente.ativa" name="check-button" switch>
-                </b-form-checkbox>
-              </div>
+          <b-collapse :id="'b-componente-drop-coplase-' + item.id">
+            <div
+              class="mt-2 mx-5 p-2 d-flex"
+              :class="
+                EstiloComponenteAtivo(
+                  'b-componente-drop-coplase-class' + item.id + componente.id,
+                  componente.ativo
+                )
+              "
+              v-for="(componente, index) in item.componente"
+              :key="index"
+            >
+              <b-form-checkbox
+                @change="
+                  desativaComponente(
+                    item.id,
+                    componente,
+                    'b-componente-drop-coplase-' + item.id + componente.id
+                  )
+                "
+                :checked="componente.ativo"
+                switch
+              >
+              </b-form-checkbox>
+              <img
+                @click="sobeComponente(item, index)"
+                width="30px"
+                height="30px"
+                class="px-1 ml-3 float-right"
+                src="~/assets/icones/btn-arrow.png"
+                alt=""
+              />
+              <img
+                width="30px"
+                height="30px"
+                @click="desceComponente(item, index)"
+                class="px-1 mr-3 text-right rotate-180"
+                src="~/assets/icones/btn-arrow.png"
+                alt=""
+              />
+              <p class="card-text mr-4 text-left">
+                {{ componente.tipoComponente }} 
+              </p>
             </div>
-            <b-collapse :id="'collapse-componentes-' + item.id" class="mt-2">
-              <b-card>Hello!</b-card>
-            </b-collapse>
           </b-collapse>
         </Drag>
       </template>
@@ -72,33 +107,93 @@ export default {
   },
   watch: {
     paginas: function (newVal, oldVal) {
-      this.paginasPainel = newVal.slice();
+      if (newVal != null) {
+        this.paginasPainel = JSON.parse(JSON.stringify(newVal));
+      }
     },
     paginasPainel: function (newVal, oldVal) {
       if (newVal != oldVal) {
-      return;
+        return;
       }
-       this.reorderPaginas(newVal);
+      this.reorderPaginas(newVal);
     },
   },
   methods: {
     ...mapActions({
       getPaginas: "paginas/getPaginas",
       salvarAlteracoes: "paginas/salvarAlteracoes",
-      autenticatorUser:'usuario/AutenticatorUser'
+      autenticatorUser: "usuario/AutenticatorUser",
     }),
     ...mapMutations({
       reorderPaginas: "paginas/REORDER_PAGINAS",
-      desativaPagina: "paginas/DESATIVA_PAGINAS",
+      desativaPaginaVuex: "paginas/DESATIVA_PAGINAS",
+      desativaComponenteVuex: "paginas/DESATIVA_COMPONENTE",
+      ordenaComponente: "paginas/ORDDENA_COMPONENTE",
     }),
+    sobeComponente(pagina, index) {
+      let pagIndex = this.paginasPainel.findIndex((p) => p == pagina);
+      if (index > 0) {
+        this.paginasPainel[pagIndex].componente[index].ordem = index - 1;
+        this.paginasPainel[pagIndex].componente[index-1].ordem = index + 1;
+        this.paginasPainel[pagIndex].componente.forEach((element) => {
+        });
+      } else {
+        this.paginasPainel[pagIndex].componente.forEach((element,posicao) => {
+          if (posicao!=0) {
+            this.paginasPainel[pagIndex].componente[posicao].ordem = index;
+          }
+        });
+      }
+      this.paginasPainel[pagIndex].componente = this.paginasPainel[
+        pagIndex
+      ].componente.sort((c1, c2) => {
+        return c1.ordem - c2.ordem;
+      });
+        this.ordenaComponente(this.paginasPainel);
+    },
+    desceComponente(pagina, index) {
+      let pagIndex = this.paginasPainel.findIndex((p) => p == pagina);
+      if (index < this.paginasPainel[pagIndex].componente.length) {
+        this.paginasPainel[pagIndex].componente[index].ordem = index + 1;
+        this.paginasPainel[pagIndex].componente[index+1].ordem = index - 1;
+      } else {
+        this.paginasPainel[pagIndex].componente.forEach((element,posicao) => {
+          if (posicao!=0) {
+            this.paginasPainel[pagIndex].componente[posicao].ordem = index;
+          }
+        });
+      }
+      this.paginasPainel[pagIndex].componente = this.paginasPainel[
+        pagIndex
+      ].componente.sort((c1, c2) => {
+        return c1.ordem - c2.ordem;
+      });
 
+        this.ordenaComponente(this.paginasPainel);
+
+    },
+    desativaComponente(paginaId, comp, idHtml) {
+      if (comp.ativo == false) {
+        $("." + idHtml).addClass("bg-danger");
+      } else {
+        $("." + idHtml).removeClass("bg-danger");
+      }
+      this.desativaComponenteVuex({ paginaId, comp });
+    },
+    EstiloComponenteAtivo(idClasseComponente, compAtivado) {
+      if (compAtivado == false) {
+        return [idClasseComponente, "bg-danger"];
+      } else {
+        return [idClasseComponente, "bg-secondary", "bg-gradient"];
+      }
+    },
     desativaPag(pagina, idHtml) {
       if (pagina.ativa == true) {
         $("#" + idHtml).addClass("bg-danger");
       } else {
         $("#" + idHtml).removeClass("bg-danger");
       }
-      this.desativaPagina(pagina);
+      this.desativaPaginaVuex(pagina);
     },
     EstiloPaginaAtiva(ativa) {
       if (ativa == false) {
